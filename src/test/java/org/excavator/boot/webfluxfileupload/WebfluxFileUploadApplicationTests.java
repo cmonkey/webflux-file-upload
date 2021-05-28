1f864594e08c6798fbfb04074a9f128a81ddb681
CMonkey <42.codemonkey@gmail.com>
2021-05-28 09:31:47 +0800

    init project

---
+++ src/test/java/org/excavator/boot/webfluxfileupload/WebfluxFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.webfluxfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
